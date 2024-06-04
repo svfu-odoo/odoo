bc48b17bab6d93ea9de4bc04d4b35cb3096db350
Sven Fuehr <svfu@odoo.com>
2024-06-04 17:21:35 +0200

    [IMP] account: rework lock dates

---
+++ addons/account/models/account_lock_exception.py
@@ -0,0 +1,167 @@
+from odoo import _, api, Command, fields, models
+
+from odoo.tools import create_index
+
+
+class AccountLockException(models.Model):
+    _name = "account.lock_exception"
+    _description = "Account Lock Exception"
+    _inherit = ['mail.thread.main.attachment', 'mail.activity.mixin']
+
+    state = fields.Selection([
+         ('active', 'Active'),
+         ('revoked', 'Revoked'),
+        ],
+        string="State",
+        tracking=True,
+        default='active',
+        required=True,
+        readonly=True,
+    )
+
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        required=True,
+        readonly=True,
+        default=lambda self: self.env.company,
+    )
+
+    # An exception w/o user_id is an exception for everyone
+    user_id = fields.Many2one(
+        'res.users',
+        string='User',
+        default=lambda self: self.env.user,
+    )
+
+    reason = fields.Char(
+        string='Reason',
+    )
+
+    start_datetime = fields.Datetime(
+        'Start Date',
+        default=fields.Datetime.now,
+        required=True,
+    )
+
+    # An exception without `end_datetime` is valid forever
+    end_datetime = fields.Datetime(
+        'End Date',
+    )
+
+    # Lock date fields; c.f. res.company
+    # An unset lock date field means the exception does not change this field.
+    # (It is not possible to remove a lock date completely).
+
+    fiscalyear_lock_date = fields.Date(
+        string="Everyone Lock Date",
+        help="The date the Everyone Lock Date is set to by this exception. If no date is set the lock date is not changed."
+    )
+
+    tax_lock_date = fields.Date(
+        string="Tax Return Lock Date",
+        help="The date the Tax Lock Date is set to by this exception. If no date is set the lock date is not changed."
+    )
+
+    sale_lock_date = fields.Date(
+        string='Lock Sales',
+        help="The date the Sale Lock Date is set to by this exception. If no date is set the lock date is not changed."
+    )
+
+    purchase_lock_date = fields.Date(
+        string='Lock Purchases',
+        help="The date the Purchase Lock Date is set to by this exception. If no date is set the lock date is not changed."
+    )
+
+    last_modified_move_ids = fields.Many2many(
+        string='Modified Journal Entries',
+        comodel_name='account.move',
+        compute='_compute_last_modified_move_ids',
+        help="Journal Entries last modified while the exception was valid",
+    )
+
+    def init(self):
+        super().init()
+        create_index(self.env.cr,
+                     indexname='account_lock_exception_id_company_id_idx',
+                     tablename=self._table,
+                     expressions=['company_id', 'start_datetime', 'end_datetime'])
+
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = _("Lock Date Exception %s", record.id)
+
+    def _compute_last_modified_move_ids(self):
+        for exception in self:
+            last_modified_moves = self.env['account.move'].search(exception._get_moves_last_modified_during_domain())
+            exception.last_modified_move_ids = [Command.set(last_modified_moves.ids)]
+
+    def _get_moves_last_modified_during_domain(self):
+        self.ensure_one()
+        domain = [
+            ('write_date', '>=', self.start_datetime),
+        ]
+        if self.end_datetime:
+            domain.append(('write_date', '<=', self.end_datetime))
+        if self.user_id:
+            domain.append(('write_uid', '=', self.user_id.id))
+        return domain
+
+    def action_show_moves_last_modified_during(self):
+        self.ensure_one()
+        return {
+            'name': _("Journal entries last modified during the exception"),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'tree',
+            'res_model': 'account.move',
+            'domain': self._get_moves_last_modified_during_domain(),
+        }
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        exceptions = super().create(vals_list)
+        for exception in exceptions:
+            company = exception.company_id
+            changed_locks = [
+                 (field, exception[field])
+                 for field in [
+                    'fiscalyear_lock_date',
+                    'tax_lock_date',
+                    'sale_lock_date',
+                    'purchase_lock_date',
+                 ]
+                 if exception[field]
+               ]
+            tracking_value_ids = []
+            for field, value in changed_locks:
+                field_info = exception.fields_get([field])[field]
+                tracking_values = self.env['mail.tracking.value']._create_tracking_values(
+                    company[field], value, field, field_info, exception
+                )
+                tracking_value_ids.append([0, 0, tracking_values])
+            end_datetime_string = _("until %s", exception.end_datetime) if exception.end_datetime else _("forever")
+            reason_string = _(" for '%s'", exception.reason) if exception.reason else ""
+            company_chatter_message = _(
+                "%(exception)s for %(user)s valid %(end_datetime_string)s%(reason)s.",
+                exception=exception._get_html_link(title=_("Exception")),
+                user=exception.user_id._get_html_link() if exception.user_id else _("everyone"),
+                end_datetime_string=end_datetime_string,
+                reason=reason_string,
+            )
+            company.sudo().message_post(
+                body=company_chatter_message,
+                tracking_value_ids=tracking_value_ids,
+            )
+        return exceptions
+
+    def action_reactivate(self):
+        """Resets a not 'active' exception back to 'active'."""
+        for record in self:
+            if record.state != 'active':
+                record.state = 'active'
+
+    def action_revoke(self):
+        """Revokes an active exception."""
+        for record in self:
+            if record.state == 'active':
+                record.state = 'revoked'
