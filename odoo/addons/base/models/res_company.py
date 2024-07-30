204c5281a63e4e63dee18ec7f97826f9f623d07f
Sven Fuehr <svfu@odoo.com>
2024-07-30 10:40:45 +0200

    [IMP] base: add search to computed field partner_ids on res.company
    There is a computed field `partner_ids` on the company.
    Since it is computed and not searchable it cannot be used as a
    dependency of compute functions.
    
    This functionality is needed in another commit of this PR though.
    
    This commit adds a search function to `partner_ids`.

--- odoo/addons/base/models/res_company.py
+++ odoo/addons/base/models/res_company.py
@@ -37,7 +37,7 @@ class Company(models.Model):
     child_ids = fields.One2many('res.company', 'parent_id', string='Branches')
     all_child_ids = fields.One2many('res.company', 'parent_id', context={'active_test': False})
     parent_path = fields.Char(index=True)
-    parent_ids = fields.Many2many('res.company', compute='_compute_parent_ids', compute_sudo=True)
+    parent_ids = fields.Many2many('res.company', compute='_compute_parent_ids', compute_sudo=True, search='_search_parent_ids')
     root_id = fields.Many2one('res.company', compute='_compute_parent_ids', compute_sudo=True)
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     report_header = fields.Html(string='Company Tagline', translate=True, help="Company tagline, which is included in a printed document's header or footer (depending on the selected layout).")
@@ -115,6 +115,10 @@ class Company(models.Model):
         for company in self.with_context(active_test=False):
             company.parent_ids = self.browse(int(id) for id in company.parent_path.split('/') if id) if company.parent_path else company
             company.root_id = company.parent_ids[0]
+
+    def _search_parent_ids(self, operator, value):
+        assert operator in ('=', 'in')
+        return [('id', 'parent_of', value)]
 
     @api.depends(lambda self: [f'partner_id.{fname}' for fname in self._get_company_address_field_names()])
     def _compute_address(self):
