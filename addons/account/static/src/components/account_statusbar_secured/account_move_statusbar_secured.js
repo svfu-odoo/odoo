a0bec72204a740b44779af74de77c0d5ce19558c
Sven Fuehr <svfu@odoo.com>
2024-08-28 11:50:48 +0200

    [IMP] account,l10n_de: new "secured" group and related UI
    This commit introduces a new user group 'account.group_account_secured'.
    When active we show
      * A lock next to the "posted" state on the moves
      * A "secured" checkbox on the moves
      * Filter "Not Secured" on moves (only includes posted moves)
      * The menuitem to open the "Secure Entries Wizard" (see previous commit of this PR)
    
    It will automatically be activated in the following cases
    (for all users of groups `account.group_account_readonly` and `account.group_account_invoice`):
      * The option on a journal to hash entries on post is enabled
        (field `restrict_mode_hash_table`).
      * The Secure Lock Entries wizard (see previous commit of this PR) is used to secure entries
        (model 'account.secure.entries.wizard').
      * Module 'l10n_de' is installed
    
    task-4095970

---
+++ addons/account/static/src/components/account_statusbar_secured/account_move_statusbar_secured.js
@@ -0,0 +1,24 @@
+import { _t } from "@web/core/l10n/translation";
+import { registry } from "@web/core/registry";
+import { statusBarField, StatusBarField } from "@web/views/fields/statusbar/statusbar_field";
+
+export class AccountMoveStatusBarSecuredField extends StatusBarField {
+    static template = "account.MoveStatusBarSecuredField";
+
+    get isSecured() {
+        return this.props.record.data['secured'];
+    }
+
+    getCurrentItem() {
+        return this.getAllItems().find((item) => item.isSelected);
+    }
+}
+
+export const accountMoveStatusBarSecuredField = {
+    ...statusBarField,
+    component: AccountMoveStatusBarSecuredField,
+    displayName: _t("Status with secured indicator for Journal Entries"),
+    supportedTypes: ["state"],
+};
+
+registry.category("fields").add("account_move_statusbar_secured", accountMoveStatusBarSecuredField);
