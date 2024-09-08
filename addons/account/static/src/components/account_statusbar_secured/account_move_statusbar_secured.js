f21c0c32adf4265cac718f0424cf9da7726d11b4
Sven Fuehr <svfu@odoo.com>
2024-09-08 23:10:44 +0200

    [IMP] account,l10n_de: [SQUASH] new "secured" group and related UI

--- addons/account/static/src/components/account_statusbar_secured/account_move_statusbar_secured.js
+++ addons/account/static/src/components/account_statusbar_secured/account_move_statusbar_secured.js
@@ -1,16 +1,24 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
+import { escape } from "@web/core/utils/strings";
 import { statusBarField, StatusBarField } from "@web/views/fields/statusbar/statusbar_field";
+import { markup } from "@odoo/owl";
 
 export class AccountMoveStatusBarSecuredField extends StatusBarField {
-    static template = "account.MoveStatusBarSecuredField";
 
     get isSecured() {
         return this.props.record.data['secured'];
     }
 
-    getCurrentItem() {
-        return this.getAllItems().find((item) => item.isSelected);
+    getAllItems() {
+        const items = super.getAllItems();
+        for (const item of items) {
+            if (item.value == 'posted') {
+                const lock_classes = this.isSecured ? 'fa-lock text-success' : 'fa-unlock text-warning'
+                item.label = markup(`${escape(item.label)}<i class="fa fa-fw ms-1 ${lock_classes}"/>`)
+            }
+        }
+        return items;
     }
 }
 
